7f226b57229a9e2e34a7963d2446ca5752e5103f
Erick Salas <63004636+ericksalas11@users.noreply.github.com>
2020-10-10 01:09:08 -0500

    Añadiendo ejercicio sobre un cajero automático
    El programa consiste en hacer operaciones naturales en un cajero automático, como por ejemplo: registrarse con usuario y clave, depositar, retirar y ver estado de cuenta.
    
    Este programa tiene por defecto unos valores para aquellas acciones con los que podemos variar:
    usuario: pepe
    clave: 12345
    saldo:10000

---
+++ cajero-automatico.py
@@ -0,0 +1,69 @@
+# DATOS
+usuarioEnBCP = "Pepe"
+claveEnBCP = "12345"
+sesion = False
+saldo = 100000
+iniciar = True
+def validarUsuario(usuario, clave) :
+   global sesion
+   if (usuario == usuarioEnBCP and clave == claveEnBCP) :
+     sesion = True
+     return True
+   return False
+def login() :
+   global sesion
+   if (sesion) :
+     return True
+   usuario = input("Digite usuario: ")
+   clave = input("Digite contraseña: ")
+   return validarUsuario(usuario, clave)
+def retirar(valor) :
+   global saldo
+   if (valor > saldo) :
+     return False, saldo
+   saldo = saldo - valor
+   return True, saldo
+def depositar(valor) :
+   global saldo
+   saldo = saldo + valor
+   return True, saldo
+def consultarSaldo() :
+   return True, saldo
+def accion(opcion) :
+   if (opcion == 1) :
+     valor = int(input("Digite el valor a depositar: "))
+     return depositar(valor)
+  
+   if (opcion == 2) :
+     valor = int(input("Digite el valor a retirar: "))
+     return retirar(valor)
+   if (opcion == 3) :
+     return consultarSaldo()
+  
+   return False, saldo
+#EJECUTANDO LA FUNCION PRINCIPAL
+
+def ejecutar() :
+   if not login() :
+     print("usuario o contraseña inválido")
+     return
+   print("¿Qué desea hacer?")
+   opcion = int(input("1 => Depositar, 2 => Retirar y 3 => Consultar saldo: "))
+  
+   ok, saldo = accion(opcion)
+   if not ok :
+     print("No se realizó la acción, saldo:", saldo)
+   else:
+     print("Acción realizada correctamente, saldo:", saldo)
+print("Bienvenido")
+
+#Pantalla de interfaz
+while (iniciar == True) :
+   ejecutar()
+   respuesta = input("¿Deseas realizar otra operación? (SI => s, y NO => valor diferete de s): ")
+   if (respuesta == "s" or respuesta == "si" or respuesta == "S" or respuesta == "SI") :
+     iniciar = True
+   else:
+     iniciar = False
+     print("Gracias por utilizar los servicios del Banco de Credito del Peru...")
+    
