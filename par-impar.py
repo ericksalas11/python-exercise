1cfac61f25e2add0cc88a47a1840123685e1ca45
Erick Salas <63004636+ericksalas11@users.noreply.github.com>
2020-10-10 01:17:09 -0500

    Añadiendo programa de par o impar
     El programa el cual te da unos números establecidos y te dice cual es par e impar respectivamente.

---
+++ par-impar.py
@@ -0,0 +1,9 @@
+print("Los numeros son: 4, 12, 15, 51, 563, 10 ")
+numbers = [4, 12, 15, 51, 563, 10]
+for numero in numbers:
+    if numero % 2 == 0:
+        print("Es par")
+    else:
+        print("Es impar")
+
+print("Respectivamente")
